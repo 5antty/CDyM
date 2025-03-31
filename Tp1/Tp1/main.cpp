0ff5c40e6fff4dad4d6752a009fab81710de8e67
Sxntty <svillagra583@gmail.com>
2025-03-30 21:07:57 -0300

    Ej1 entregale hecho

--- Tp1/Tp1/main.cpp
+++ Tp1/Tp1/main.cpp
@@ -3,31 +3,32 @@
  *
  * Created: 28/03/2025 18:59:25
  * Author : santi
- */ 
+ */
 
-/* Inclusión de bibliotecas de código */
-#include <avr/io.h> // Registros del microcontrolador
+/* Inclusiï¿½n de bibliotecas de cï¿½digo */
+#include <avr/io.h>		 // Registros del microcontrolador
 #define F_CPU 16000000UL // Defino la frecuencia de oscilador en 8MHz
-#include <util/delay.h> // Retardos por software
+#include <util/delay.h>	 // Retardos por software
 
 /*Funciones de secuencias de leds*/
 void Sec1(void)
 {
 	DDRD = 0xFF;
 	PORTD = 0b10101010;
-	_delay_ms(100); // Delay de 100 ms
+	_delay_ms(200); // Delay de 100 ms
 	PORTD = 0x00;
 	PORTD = 0b01010101;
-	_delay_ms(100); // Delay de 100 ms
+	_delay_ms(200); // Delay de 100 ms
 	PORTD = 0x00;
 }
 void Sec2(void)
 {
 	char aux = 0x01;
 	DDRD = aux;
-	int i=0;
+	int i = 0;
 	PORTD = 0x01;
-	for (i=0;i<8;i++){
+	for (i = 0; i < 8; i++)
+	{
 		_delay_ms(100); // Delay de 100 ms
 		DDRD = 0x00;
 		PORTD <<= 1;
@@ -36,17 +37,54 @@ void Sec2(void)
 	}
 }
 
-/* Función main */
-int main (void)
+void Sec3(void)
+{
+	char aux = 0x80;
+	DDRD = aux;
+	int i = 0;
+	PORTD = 0x80;
+	for (i = 0; i < 8; i++)
+	{
+		_delay_ms(100); // Delay de 100 ms
+		DDRD = 0x00;
+		PORTD >>= 1;
+		aux >>= 1;
+		DDRD = aux;
+	}
+	Sec2();
+}
+
+/* Funciï¿½n main */
+int main(void)
 {
 	/* Setup */
 	/* Loop */
-	while(1)
+
+	int cont=0;
+	DDRC=0x00;
+	while (1)
 	{
-		Sec1();
-		_delay_ms(100);
-		Sec2();
+		if(PORTC0 && (cont<3)){
+			cont++;
+			_delay_ms(200);
+		}
+		else if (cont>=3){
+			cont=0;
+			_delay_ms(200);
+		}
+		if(PORTC0 &&(cont==0)){
+			Sec1();
+			_delay_ms(200);
+		}
+		if(PORTC0 &&(cont==1)){
+			Sec2();
+			_delay_ms(200);
+		}
+		if(PORTC0 &&(cont==2)){
+			Sec3();
+			_delay_ms(200);
+		}
 	}
-	/* Punto de finalización del programa (NO se debe llegar a este lugar) */
+	/* Punto de finalizaciï¿½n del programa (NO se debe llegar a este lugar) */
 	return 0;
 }
