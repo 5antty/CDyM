8268212c5e81b6361d86a8d46ee0b8aeb10d091d
Sxntty <svillagra583@gmail.com>
2025-07-31 23:36:44 -0300

    terminado?

--- TP4/TP4/main.c
+++ TP4/TP4/main.c
@@ -11,55 +11,67 @@
 
 uint8_t FlagADC=0;
 uint8_t FlagUART=0;
+uint8_t R=255;
+uint8_t G=255;
+uint8_t B=255;
 
 char BufferRX[TamBuffers];
 uint8_t result=0;
-uint8_t color=0;
 uint8_t colorSoft=0;
-float porc;
+
+uint8_t brillo(uint8_t ccolor, uint8_t adcv)
+{
+	return 255 - ((255 - ccolor) * adcv) / 255;
+}
+
 void colores(){
-	adc_start();
-	while(!FlagADC);
-	color=result;
 	//actualizo el ciclo de trabajo fuera de la uart
 	//la uart solo me cambia los flags de los colores a actualizar
 	switch(BufferRX[0]){
-		case '1':
-			colorSoft=0;
-			PWM_T1_update(color, color);
+		case '1'://ROJO
+			R=0;
+			G=255;
+			B=255;
 		break;
-		case '2':
-			colorSoft=color;
-			PWM_T1_update(0, color);
+		case '2'://VERDE
+			R=255;
+			G=0;
+			B=255;
 		break;
-		case '3':
-			colorSoft=color;
-			PWM_T1_update(color, 0);
+		case '3'://AZUL
+			R=255;
+			G=255;
+			B=0;
 		break;
-		case '4':
-			colorSoft=color;
-			PWM_T1_update(0, 0);
+		case '4'://CIAN
+			R=255;
+			G=0;
+			B=0;
 		break;
-		case '5':
-			colorSoft=0;
-			PWM_T1_update(0, color);
+		case '5'://AMARILLO
+			R=0;
+			G=0;
+			B=255;
 		break;
-		case '6':
-			colorSoft=0;
-			PWM_T1_update(color, 0);
+		case '6'://MAGENTA
+			R=0;
+			G=255;
+			B=0;
 		break;
-		case '7':
-			colorSoft=0;
-			PWM_T1_update(0, 0);
+		case '7'://BLANCO
+			R=0;
+			G=0;
+			B=0;
 		break;
-		case '8':
-			colorSoft=color;
+		case '8'://NEGRO
+			R=255;
+			G=255;
+			B=255;
 		break;
 	}
 }
 
 void initALL(){
-	
 	uart_init();
 	uart_TXEnable();
 	uart_sendString("Ingrese el color\r\n1=RED\r\n2=GREEN\r\n3=BLUE\r\n4=CIAN\r\n5=YELLOW\r\n6=MAGENTA\r\n7=WHITE\r\n8=BLACK\r\n");
@@ -71,7 +83,6 @@ void initALL(){
 	DDRB=0xff;
 }
 
-
 int main(void)
 {
 	
@@ -81,15 +92,13 @@ int main(void)
     {
 		if(FlagUART){
 			FlagUART=0;
-			
+			colores();
 		}
-		colores();
-		if(FlagADC){
-			FlagADC=0;
-			color=result;
-		}
-		
-		_delay_ms(100);
+		adc_start();
+		while(!FlagADC);
+		FlagADC=0;
+		colorSoft=brillo(R, result);
+		PWM_T1_update(brillo(G, result), brillo(B, result));
     }
 	return 0;
 }
